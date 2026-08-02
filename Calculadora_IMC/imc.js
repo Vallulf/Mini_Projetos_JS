17288b6c23e2c134c3ca9b0d6d855074018d7fe5
Vallulf <waltersoares916@gmail.com>
2026-08-02 18:03:10 -0300

    att

---
+++ Calculadora_IMC/imc.js
@@ -0,0 +1,6 @@
+ let nome = document.getElementById('nome')
+ let altura = document.getElementById('altura')
+ let peso = document.getElementById('peso')
+ let calcular = document.getElementById('calcular')
+ let resultado = document.getElementById('resultado')
+ 
