63a6e5954887a7c83138a13916646bfc56349021
Vallulf <waltersoares916@gmail.com>
2026-08-09 02:09:09 -0300

    att

--- Sistema_Cadastro/Api.js
+++ Sistema_Cadastro/Api.js
@@ -1,7 +1,8 @@
-let supabaseUrl= 'https://tgqzzdayhuorkntmkhvu.supabase.co'
-let supabaseKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRncXp6ZGF5aHVvcmtudG1raHZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyMzUwNDMsImV4cCI6MjEwMTgxMTA0M30.QwwmN6Wtsj7GgYxGjEyfzYtENprAzMbkyeUyJnT08-U'
+let supabaseUrl = 'https://tgqzzdayhuorkntmkhvu.supabase.co'
 
-let supabase = window.supabase.CreateClient(
+let supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRncXp6ZGF5aHVvcmtudG1raHZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyMzUwNDMsImV4cCI6MjEwMTgxMTA0M30.QwwmN6Wtsj7GgYxGjEyfzYtENprAzMbkyeUyJnT08-U'
+
+let supabaseClient = window.supabase.createClient(
     supabaseUrl,
     supabaseKey
 )
