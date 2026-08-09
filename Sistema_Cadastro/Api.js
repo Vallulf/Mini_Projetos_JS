04d2d95226b66fc74b1b543bf48f3495dcab8b18
Vallulf <waltersoares916@gmail.com>
2026-08-09 01:46:16 -0300

    att

---
+++ Sistema_Cadastro/Api.js
@@ -0,0 +1,7 @@
+let supabaseUrl= 'https://tgqzzdayhuorkntmkhvu.supabase.co'
+let supabaseKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRncXp6ZGF5aHVvcmtudG1raHZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyMzUwNDMsImV4cCI6MjEwMTgxMTA0M30.QwwmN6Wtsj7GgYxGjEyfzYtENprAzMbkyeUyJnT08-U'
+
+let supabase = window.supabase.CreateClient(
+    supabaseUrl,
+    supabaseKey
+)
